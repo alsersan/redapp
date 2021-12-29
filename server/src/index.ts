7557b97700e2b4ba71b376c3fc3907750d4b76eb
Álvaro Serrano <alsersan20@gmail.com>
2021-12-29 17:56:13 +0100

    Add Post entity

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,10 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
+import { Post } from './entities/Post';
 
 (async () => {
   const orm = await MikroORM.init({
-    entities: [],
+    entities: [Post],
     dbName: 'redapp',
     type: 'postgresql',
     debug: !__prod__,
