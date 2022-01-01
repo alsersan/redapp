417fe4b66f958894ee288578a7c8e6c059733add
Álvaro Serrano <alsersan20@gmail.com>
2022-01-01 20:13:18 +0100

    Add config for mikroORM CLI

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,6 @@
 import { MikroORM } from '@mikro-orm/core';
-import { __prod__ } from './constants';
-import { Post } from './entities/Post';
+import mikroConfig from './mikro-orm.config';
 
 (async () => {
-  const orm = await MikroORM.init({
-    entities: [Post],
-    dbName: 'redapp',
-    type: 'postgresql',
-    debug: !__prod__,
-  });
+  const orm = await MikroORM.init(mikroConfig);
 })();
