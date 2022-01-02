40c44ba6080eaf82890c2ac3272e173a591d2c34
Álvaro Serrano <alsersan20@gmail.com>
2022-01-02 21:17:54 +0100

    Add read single post by id

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,11 +1,16 @@
 import { Post } from '../entities/Post';
 import { MyContext } from '../types';
-import { Ctx, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Int, Query, Resolver } from 'type-graphql';
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  post(@Ctx() { em }: MyContext): Promise<Post[]> {
+  getAllPosts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  getPost(@Arg('id', () => Int) id: number, @Ctx() { em }: MyContext): Promise<Post | null> {
+    return em.findOne(Post, { id });
   }
 }
